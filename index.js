ab3df36947a30c972f0d4ea151902e1f87834c16
ashfaque9426 <ashfaqsylvi9426@gmail.com>
2023-06-11 06:49:06 +0600

    added addAClass api

--- index.js
+++ index.js
@@ -287,6 +287,32 @@ async function run() {
 
         });
 
+        // add a class api so do update required fields
+        app.post('addAClass/:email', verifyJWT, async(req, res) => {
+            const decodedEmail = req.decoded.email;
+            const email = req.params.email;
+            const addedClass = req.body;
+            const instructorEmail = req.body.email;
+            const className = req.body.className;
+
+            if(decodedEmail !== email) return res.status(403).send({error: true, message: "Forbidden Access"});
+
+            const query = { email: instructorEmail };
+
+            await allUsersCollection.updateOne(query, { $push: { nameOfClasses: className } });
+            const findUser = await allUsersCollection.findOne(query);
+            const newNumberOfClass = findUser.nameOfClasses.length > 0 ? findUser.nameOfClasses.length : 1;
+            const updateDoc = {
+                $set: {
+                    numberOfClasses: newNumberOfClass
+                }
+            }
+            await allUsersCollection.updateOne(query, updateDoc);
+
+            const result = await allClasses.insertOne(addedClass);
+            res.send(result);
+        });
+
         app.delete('/studentsClass/:id/:email', verifyJWT, async(req, res)=> {
             const decodedEmail = req.decoded.email;
             const email = req.params.email;
